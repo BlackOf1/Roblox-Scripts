6111af6f35ac9289547ec2d03dea5cee604510f1
denzel980 <35507814+denzel980@users.noreply.github.com>
2021-04-08 18:34:14 -0400

    Update Ken Omega.lua

--- Ken Omega.lua
+++ Ken Omega.lua
@@ -6,14 +6,190 @@ function Notification(Msg)
     })
 end 
 
+local Run = nil
+local StopRun = nil
+
+for _, v in next, getgc() do
+    if typeof(v) == "function" then
+    local script = getfenv(v).script
+        if script and typeof(script) == "Instance" then
+            if script == Script then
+                if debug.getinfo(v).name == "runPrompt" then 
+                 Run = v
+                elseif debug.getinfo(v).name == "stopSprint" then 
+                    StopRun = v
+                end 
+            end
+        end
+    end
+end
+--local Bar = game:GetService("Players").fordnikefox.PlayerGui.MainGui.Utility.StamBar.BarF.Bar
+local function CanSpeed()
+    if game:GetService("Players").fordnikefox.PlayerGui.MainGui.Utility.StamBar.BarF.Bar.AbsoluteSize.X < 115 then 
+        return false 
+    elseif game:GetService("Players").fordnikefox.PlayerGui.MainGui.Utility.StamBar.BarF.Bar.AbsoluteSize.X > 535 then
+        return true 
+    end 
+end
+
+
+
+local function WalkTo(destination,state,CanRun,Getfood)
+   local Char =  game.Players.LocalPlayer.Character or game.Players.LocalPlayer.CharacterAdded:Wait()
+    local path = PathfindingService:CreatePath()
+
+    path:ComputeAsync(Char("HumanoidRootPart").Position, destination)
+    local humanoid = Char:WaitForChild("Humanoid")
+
+    local waypoints = path:GetWaypoints()
+    local Money = tonumber(string.sub(game:GetService("Players").LocalPlayer.PlayerGui.MainGui.Utility.Money.Text,2))
+
+    for _, waypoint in pairs(waypoints) do
+        local CombatTag = game:GetService("Players").LocalPlayer.PlayerGui.MainGui.Utility.CombatTag.Visible
+        if CanSpeed() == true and CanRun == true then 
+            Run()
+        else 
+            StopRun()
+        end 
+        humanoid:MoveTo(waypoint.Position)
+        humanoid.MoveToFinished:Wait()
+        if CombatTag == true and state == true and Money > 600 and CanRun == true and shared.MoneyFarmed == true then
+            local Bank = game:GetService("Workspace").Bank.Part
+            local BankLoc = Bank.Position
+            WalkTo(BankLoc,false,true)
+           local Click = Bank.Parent.ClickDetector
+           fireclickdetector(Click)
+           for i,v in pairs(getconnections(game:GetService("Players").LocalPlayer.PlayerGui.BankGUI.Frame.Deposit.MouseButton1Click)) do 
+                local Money = string.sub(game:GetService("Players").LocalPlayer.PlayerGui.MainGui.Utility.Money.Text,2)
+                game:GetService("Players").fordnikefox.PlayerGui.BankGUI.Frame.Amount.Text = Money
+                wait(.5)
+                v:Fire()
+            end
+        elseif Money > 3500 then
+            local Bank = game:GetService("Workspace").Bank.Part
+            local BankLoc = Bank.Position
+            WalkTo(BankLoc,false,true)
+           local Click = Bank.Parent.ClickDetector
+           fireclickdetector(Click)
+           for i,v in pairs(getconnections(game:GetService("Players").LocalPlayer.PlayerGui.BankGUI.Frame.Deposit.MouseButton1Click)) do 
+                local Money = string.sub(game:GetService("Players").LocalPlayer.PlayerGui.MainGui.Utility.Money.Text,2)
+                game:GetService("Players").fordnikefox.PlayerGui.BankGUI.Frame.Amount.Text = Money
+                wait(.5)
+                v:Fire()
+            end
+        elseif game:GetService("Players").fordnikefox.PlayerGui.MainGui.Utility.StamBar.BarF.Bar.AbsoluteSize.X < 61 and CombatTag == false and Money > 300 and Getfood == true and game:GetService("Players").fordnikefox.Backpack:FindFirstChild("Chicken Fries") == nil and shared.MoneyFarmed == true and shared.MoneyFarmed == true then 
+            local Food = Vector3.new(-1125.52173, 47.2412643, -294.940125)
+            MoveTo(Food,true,false)
+            for count = 0, 5,1  do 
+                fireclickdetector(game:GetService("Workspace")["Chicken Fries: $20"].ClickDetector)
+            end 
+            for i,v in pairs(game:GetService("Players").fordnikefox.Backpack:GetChildren()) do  
+                if v.Name == "Chicken Fries" and v:IsA("Tool") then 
+                    humanoid:EquipTool(v) 
+                    v:Activate()
+                    if game:GetService("Players").fordnikefox.PlayerGui.MainGui.Utility.StamBar.BarF.Bar.AbsoluteSize.X > 220 then
+                        break
+                    end
+                end 
+            end
+        elseif shared.MoneyFarmed == false then
+            return
+        end
+        if game:GetService("Players").fordnikefox.PlayerGui.MainGui.Utility.StamBar.BarF.Bar.AbsoluteSize.X < 61 and CombatTag == false and Money > 20 and Getfood == true and game:GetService("Players").fordnikefox.Backpack:FindFirstChild("Chicken Fries") and shared.MoneyFarmed == true then
+            for i,v in pairs(game:GetService("Players").fordnikefox.Backpack:GetChildren()) do  
+                if v.Name == "Chicken Fries" and v:IsA("Tool") then 
+                    humanoid:EquipTool(v) 
+                    v:Activate()
+                    if game:GetService("Players").fordnikefox.PlayerGui.MainGui.Utility.StamBar.BarF.Bar.AbsoluteSize.X > 220 then
+                        break
+                    end
+                end 
+            end
+        elseif shared.MoneyFarmed == false then
+            return
+        end
+        WalkTo(waypoint.Position,true,true,true)
+    end 
+end
+local function DoJob()
+    local FirstSpot = game:GetService("Workspace").Jobs.SupplyDelivery.Japanese1.Part1
+    local SecondSpot = game:GetService("Workspace").Jobs.SupplyDelivery.Japanese1.Part2
+    WalkTo(FirstSpot.Position,true,true,false)
+    firetouchinterest(game:GetService("Workspace").Jobs.SupplyDelivery.Japanese1.Part1,game.Players.LocalPlayer.Character.HumanoidRootPart,0)
+    firetouchinterest(game:GetService("Workspace").Jobs.SupplyDelivery.Japanese1.Part1,game.Players.LocalPlayer.Character.HumanoidRootPart,1)
+    WalkTo(SecondSpot.Position,true,true,true)
+    firetouchinterest(game:GetService("Workspace").Jobs.SupplyDelivery.Japanese1.Part2,game.Players.LocalPlayer.Character.HumanoidRootPart,0)
+    firetouchinterest(game:GetService("Workspace").Jobs.SupplyDelivery.Japanese1.Part2,game.Players.LocalPlayer.Character.HumanoidRootPart,1)
+end
+
+
+local Script = game:GetService("Players").LocalPlayer.Backpack.LocalS
+---
+
+
 local Ui = loadstring(game:HttpGet("https://raw.githubusercontent.com/preztel/AzureLibrary/master/uilib.lua", true))()
 shared.MoneyFarmed = false
 
 local AutoFarmCate = Ui:CreateTab("Auto-Farm","",true)
 AutoFarmCate:CreateToggle("Money Farm", function(arg)
-    if arg == true and game:GetSerivce("Workspace").Live:FindFirstChild("fordnikefox") then 
-        
-    elseif game:GetSerivce("Workspace").Live:FindFirstChild("fordnikefox") == nil then 
+    local PlayerName = game.Players.LocalPlayer.Name
+    if arg == true and game:GetSerivce("Workspace").Live:FindFirstChild(PlayerName) then 
+        shared.MoneyFarmed = true 
+        if game:GetService("Players").LocalPlayer.PlayerGui:FindFirstChild("JobGUI") == nil and shared.MoneyFarmed == true  then
+            local Board = game:GetService("Workspace").JobBoardModel1.Board  
+            while shared.MoneyFarmed == true do
+                WalkTo(Board.Position,true,true,true)
+                wait()
+                repeat
+                    fireclickdetector(game:GetService("Workspace").JobBoardModel1.ClickDetector)
+                    wait(.85)
+                    if game:GetService("Players").LocalPlayer.PlayerGui:FindFirstChild("JobGUI") and shared.MoneyFarmed == true then 
+                        if game:GetService("Players").fordnikefox.PlayerGui.JobGUI.Frame.Title.Text == "Restock Job" and shared.MoneyFarmed == true then 
+                            break
+                        elseif game:GetService("Players").fordnikefox.PlayerGui.JobGUI.Frame.Title.Text ~= "Restock Job" and shared.MoneyFarmed == true  then 
+                            for i,v in pairs(getconnections(game:GetService("Players").fordnikefox.PlayerGui.JobGUI.Frame.Quit.MouseButton1Click)) do 
+                                wait(.5)
+                                v:Fire()
+                            end
+                        elseif shared.MoneyFarmed == false then 
+                            break 
+                        end 
+                    end 
+                    wait()
+                until game:GetService("Players").fordnikefox.PlayerGui:WaitForChild("JobGUI").Frame.Title.Text == "Restock Job"
+                DoJob()
+            end 
+        elseif game:GetService("Players").LocalPlayer.PlayerGui:FindFirstChild("JobGUI") and shared.MoneyFarmed == true then
+            local Board = game:GetService("Workspace").JobBoardModel1.Board  
+            while shared.MoneyFarmed == true do
+                if game:GetService("Players").LocalPlayer.PlayerGui:FindFirstChild("JobGUI") == nil or game:GetService("Players").LocalPlayer.PlayerGui:FindFirstChild("JobGUI").Text ~= "Restock Job" and shared.MoneyFarmed == true then 
+                    WalkTo(Board.Position,true,true,true)
+                end
+                wait()
+                repeat
+                    fireclickdetector(game:GetService("Workspace").JobBoardModel1.ClickDetector)
+                    wait(.85)
+                    if game:GetService("Players").LocalPlayer.PlayerGui:FindFirstChild("JobGUI") and shared.MoneyFarmed == true then 
+                        if game:GetService("Players").fordnikefox.PlayerGui.JobGUI.Frame.Title.Text == "Restock Job" and shared.MoneyFarmed == true then 
+                            break
+                        elseif game:GetService("Players").fordnikefox.PlayerGui.JobGUI.Frame.Title.Text ~= "Restock Job" and shared.MoneyFarmed == true  then 
+                            for i,v in pairs(getconnections(game:GetService("Players").fordnikefox.PlayerGui.JobGUI.Frame.Quit.MouseButton1Click)) do 
+                                wait(.5)
+                                v:Fire()
+                            end
+                        elseif shared.MoneyFarmed == false then 
+                            break 
+                        end 
+                    end 
+                    wait()
+                until game:GetService("Players").fordnikefox.PlayerGui:WaitForChild("JobGUI").Frame.Title.Text == "Restock Job"
+                DoJob()
+            end 
+        end 
+    elseif game:GetSerivce("Workspace").Live:FindFirstChild(PlayerName) == nil then 
         Notification("Your Not Loaded In The Game")
+    elseif arg == false then
+        shared.MoneyFarmed = false 
+        Notification("You Have Stopped AutoFarming")
     end 
 end)
