09c0b32086b8786c2c645c5667c2af9bdfd73deb
BlackOf <35507814+BlackOf1@users.noreply.github.com>
2021-06-30 08:17:28 -0400

    Update DragonB.lua

--- DragonB.lua
+++ DragonB.lua
@@ -29,6 +29,7 @@ spawn(function()
          local NpcShadow = game.Workspace:FindFirstChild(game.Players.LocalPlayer.Name.."'s Shadow Image")
          if game.Players.LocalPlayer.Character and NpcShadow == nil then
             game:GetService("Players").LocalPlayer.Character.Client.Events:FindFirstChild("Shadow Spar"):FireServer()
+            wait(3.5)
             NpcShadow = game.Workspace:FindFirstChild(game.Players.LocalPlayer.Name.."'s Shadow Image")
         end 
          if NpcShadow and  game:GetService("Players").LocalPlayer.Character and game.Players.LocalPlayer.Character.Config.Resting.Value == false  then 
