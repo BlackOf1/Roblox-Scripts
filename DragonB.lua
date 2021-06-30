87e9d5c690c08f1940c74624c44880ab5c9917bb
BlackOf <35507814+BlackOf1@users.noreply.github.com>
2021-06-30 09:40:45 -0400

    Update DragonB.lua

--- DragonB.lua
+++ DragonB.lua
@@ -33,7 +33,7 @@ spawn(function()
         pcall(function()
             if game:GetService("Players").LocalPlayer.Character and _G.NpcShadow then 
                 game:GetService("Players").LocalPlayer.Character.Client.Events.Block:InvokeServer()
-                wait(3.25)
+                wait(2.75)
                 game:GetService("Players").LocalPlayer.Character.Client.Events.StopBlocking:InvokeServer()
                 wait(5)
             end 
