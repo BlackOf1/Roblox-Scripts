c3c4e9dde9bf813380e8f76af0a352a1e11aa66a
BlackOf <35507814+BlackOf1@users.noreply.github.com>
2021-06-30 09:23:18 -0400

    Update DragonB.lua

--- DragonB.lua
+++ DragonB.lua
@@ -8,7 +8,7 @@ game.Workspace.FallenPartsDestroyHeight = -500000000
 spawn(function()
     while wait() do  
         pcall(function()
-            if game:GetService("Players").LocalPlayer.Character then 
+            if game:GetService("Players").LocalPlayer.Character and _G.NpcShadow then 
                 if  game.Players.LocalPlayer.Character.Config.AttackCooldown.Value == true then 
                     game.Players.LocalPlayer.Character.Config.AttackCooldown.Value = false 
                 elseif game.Players.LocalPlayer.Character.Config.CAttackCooldown.Value == true then
@@ -21,8 +21,6 @@ spawn(function()
                     game.Players.LocalPlayer.Character.Config.Flying.Value = false 
                 elseif game.Players.LocalPlayer.Character.Config.CanBlock.Value == false then 
                     game.Players.LocalPlayer.Character.Config.CanBlock.Value = true
-                elseif game.Players.LocalPlayer.Character.Config.Blocking.Value == false then 
-                    game:GetService("Players").LocalPlayer.Character.Client.Events.Block:InvokeServer()
                 elseif game.Players.LocalPlayer.Character.Config.Boosting.Value == false  then 
                     game.Players.LocalPlayer.Character.Config.Boosting.Value = true
                 end 
@@ -30,29 +28,40 @@ spawn(function()
         end)
     end
  end)
+ spawn(function()
+    while wait() do 
+        pcall(function()
+            if game:GetService("Players").LocalPlayer.Character and _G.NpcShadow then 
+                game:GetService("Players").LocalPlayer.Character.Client.Events.Block:InvokeServer()
+                wait(4.5)
+                game:GetService("Players").LocalPlayer.Character.Client.Events.Block:InvokeServer()
+                wait(4.5)
+            end 
+        end)
+    end 
+ end)
  while wait() do
      pcall(function()
-         local NpcShadow = game.Workspace:FindFirstChild(game.Players.LocalPlayer.Name.."'s Shadow Image")
-         if game.Players.LocalPlayer.Character and NpcShadow == nil then
+         local _G.NpcShadow = game.Workspace:FindFirstChild(game.Players.LocalPlayer.Name.."'s Shadow Image")
+         if game.Players.LocalPlayer.Character and _G.NpcShadow == nil then
             game:GetService("Players").LocalPlayer.Character.Client.Events:FindFirstChild("Shadow Spar"):FireServer()
             wait(1)
-            NpcShadow = game.Workspace:FindFirstChild(game.Players.LocalPlayer.Name.."'s Shadow Image")
+            _G.NpcShadow = game.Workspace:FindFirstChild(game.Players.LocalPlayer.Name.."'s Shadow Image")
         end 
-         if NpcShadow and  game:GetService("Players").LocalPlayer.Character and game.Players.LocalPlayer.Character.Config.Resting.Value == false  then 
+         if _G.NpcShadow and  game:GetService("Players").LocalPlayer.Character and game.Players.LocalPlayer.Character.Config.Resting.Value == false  then 
              game.Players.LocalPlayer.Character.Config.Resting.Value = true
             --game:GetService("Players").LocalPlayer.Character.Client.Events.Rest:FireServer()
-         elseif NpcShadow == nil then 
+         elseif _G.NpcShadow == nil then 
              game.Players.LocalPlayer.Character.Config.Resting.Value = false 
              --game:GetService("Players").LocalPlayer.Character.Client.Events.Rest:FireServer()
          end 
-         if NpcShadow and game:GetService("Players").LocalPlayer.Character then 
+         if _G.NpcShadow and game:GetService("Players").LocalPlayer.Character then 
              local args = {[1] = "SecretCode"}
              game:GetService("Players").LocalPlayer.Character.Client.Events.LightAttack:FireServer(unpack(args))
-             local Look = CFrame.lookAt(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.Position,NpcShadow.HumanoidRootPart.Position)
-             local perHrp = NpcShadow.HumanoidRootPart.CFrame * CFrame.new(0,0,-1)
+             local Look = CFrame.lookAt(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.Position,_G.NpcShadow.HumanoidRootPart.Position)
+             local perHrp = _G.NpcShadow.HumanoidRootPart.CFrame * CFrame.new(0,0,-1)
              local x, y, z, r00, r01, r02, r10, r11, r12, r20, r21, r22 = Look:components()
              local x1, y1, z1, r001, r011, r021, r101, r111, r121, r201, r211, r221 = perHrp:components()
-             --game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = perHrp  
              game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(x1,y2,z1,r00, r01, r02, r10, r11, r12, r20, r21, r22)
          end 
      end)
